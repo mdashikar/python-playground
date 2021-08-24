9d60d9e1ea92f9325aa2c1134e5f310a6479a051
Md. Ashiqur Rahman <md.ashik.ar@gmail.com>
2021-08-25 03:11:15 +0600

    Getting started with python loops, variables, tuple and list

---
+++ test.py
@@ -0,0 +1,23 @@
+# define variable
+message='Hello mars'
+
+# Print Welcome Message
+print(message)
+
+# tuple
+programing_languages = "Python", "Java", "C++", "C#"
+
+print(type(programing_languages))
+
+#loop
+
+for lang in programing_languages:
+    print(lang)
+
+#tic-tac-toe game
+game = [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],]
+
+for row in game:
+    print(row)
