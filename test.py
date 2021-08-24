c6fd7c1799276b486a1c88b4c44dbd3047643628
Md. Ashiqur Rahman <md.ashik.ar@gmail.com>
2021-08-25 04:15:54 +0600

    learned - list index, slices, built-in functions, functions

--- test.py
+++ test.py
@@ -1,23 +1,30 @@
 # define variable
-message='Hello mars'
+#message='Hello mars'
 
 # Print Welcome Message
-print(message)
+#print(message)
 
 # tuple
-programing_languages = "Python", "Java", "C++", "C#"
+# programing_languages = "Python", "Java", "C++", "C#"
 
-print(type(programing_languages))
+# print(type(programing_languages))
 
 #loop
 
-for lang in programing_languages:
-    print(lang)
+#for lang in programing_languages:
+    # print(lang)
 
 #tic-tac-toe game
 game = [[0, 0, 0],
         [0, 0, 0],
         [0, 0, 0],]
 
-for row in game:
-    print(row)
+def game_board(player=0, row=0, col=0, just_display=False):
+    print("     a  b  c")
+    if not just_display:
+        game[row][col]= player
+    for index, row in enumerate(game):
+        print(index, row)
+
+game_board(just_display=True)
+game_board(current_player = 1, row_choice = 2, col_choice=0)
